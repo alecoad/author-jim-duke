514075021417948f19a7f62a9fdb8f3ae2638397
alecoad <andrewlecoad@gmail.com>
2023-07-17 21:33:05 -0600

    Update description

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,7 +2,7 @@ module.exports = {
 	title: "Jim Duke | Author",
 	url: "https://authorjimduke.com/",
 	language: "en",
-	description: "I am writing about my experiences.",
+	description: "In a crazy world, his critters have always been his most reliable support group leading him through life. There are few problems Duke can't outrun on a fast mule.",
 	author: {
 		name: "Jim Duke",
 		email: "authorjimduke.com",
